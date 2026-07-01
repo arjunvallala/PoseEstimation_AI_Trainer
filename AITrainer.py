c5b36f77b33b438c40ec09a65e07dc50a402a97a
Arjun Vallala <vallalaarjun2006@gmail.com>
2026-07-01 10:50:13 +0530

    Change left direction logic and optimize count updates

--- AITrainer.py
+++ AITrainer.py
@@ -12,7 +12,7 @@ PoseEstimator=Pose.PoseEstimator()
 count=0
 dire=0
 right = True
-left = False
+left = True
 while True:
     success,img=cap.read()
     if not success:
@@ -33,6 +33,16 @@ while True:
             cv2.rectangle(img,(1130,int(barRight)),(1170,700),(0,255,0),cv2.FILLED)
             cv2.putText(img,f"{int(perRight)}%",(1120,170),cv2.FONT_HERSHEY_PLAIN,2,(255,255,255),2)
             # cv2.putText(img,f"{int(angle1)}",(40,60),cv2.FONT_HERSHEY_PLAIN,2,(0,255,0),2)
+            if perRight>=99:
+                if dire==0:
+                    count+=0.5
+                    dire=1
+            if perRight<=1:
+                if dire==1:
+                    count+=0.5
+                    dire=0
+            cv2.rectangle(img,(1180,600),(1280,740),(10,0,100),cv2.FILLED)
+            cv2.putText(img,str(int(count)),(1210,700),cv2.FONT_HERSHEY_PLAIN,5,(255,255,255),5)
         if left:
             perLeft=np.interp(angle2,(40,170),(0,100))
             barLeft=np.interp(angle2,(40,170),(700,200))
@@ -40,26 +50,16 @@ while True:
             cv2.rectangle(img,(110,int(barLeft)),(150,700),(0,255,0),cv2.FILLED)
             cv2.putText(img,f"{int(perLeft)}%",(100,170),cv2.FONT_HERSHEY_PLAIN,2,(255,255,255),2)
             # cv2.putText(img,f"{int(angle2)}",(40,60),cv2.FONT_HERSHEY_PLAIN,2,(0,255,0),2)
-        if perRight>=99:
-            if dire==0:
-                count+=0.5
-                dire=1
-        if perRight<=1:
-            if dire==1:
-                count+=0.5
-                dire=0
-        cv2.rectangle(img,(1180,600),(1280,740),(10,0,100),cv2.FILLED)
-        cv2.putText(img,str(int(count)),(1210,700),cv2.FONT_HERSHEY_PLAIN,5,(255,255,255),5)
-        if perLeft>=99:
-            if dire==0:
-                count+=0.5
-                dire=1
-        if perLeft<=1:
-            if dire==1:
-                count+=0.5
-                dire=0
-        cv2.rectangle(img,(0,600),(100,740),(10,0,100),cv2.FILLED)
-        cv2.putText(img,str(int(count)),(30,700),cv2.FONT_HERSHEY_PLAIN,5,(255,255,255),5)
+            if perLeft>=99:
+                if dire==0:
+                    count+=0.5
+                    dire=1
+            if perLeft<=1:
+                if dire==1:
+                    count+=0.5
+                    dire=0
+            cv2.rectangle(img,(0,600),(100,740),(10,0,100),cv2.FILLED)
+            cv2.putText(img,str(int(count)),(30,700),cv2.FONT_HERSHEY_PLAIN,5,(255,255,255),5)
     cv2.imshow("Video",img)
     if cv2.waitKey(1)&0xFF==ord('q'):
         break
